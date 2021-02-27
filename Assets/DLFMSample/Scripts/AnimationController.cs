5de4c3f609fe79e3f15ada08f2611570d44a95a0
dogdie233 <dogdie233@163.com>
2021-02-27 13:21:09 +0800

    Animation System

---
+++ Assets/DLFMSample/Scripts/AnimationController.cs
@@ -0,0 +1,41 @@
+﻿using Event;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Level.Animations
+{
+    public class AnimationController : MonoBehaviour
+    {
+        public PlayableDirector timeline;
+
+        void Start()
+        {
+            EventManager.onStateChange.AddListener(args =>
+            {
+                switch (args.newState)
+                {
+                    case Level.GameState.Playing:
+                        timeline.Play();
+                        break;
+                    case Level.GameState.WaitingRespawn:
+                        timeline.Pause();
+                        break;
+                }
+                return args;
+            }, Priority.Lowest);
+            EventManager.onRespawn.AddListener(args =>
+            {
+                timeline.time = args.crown.time;
+                return args;
+            }, Priority.Lowest);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
